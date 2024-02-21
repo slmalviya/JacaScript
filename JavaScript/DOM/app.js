293015b64ac305be97dc04d1036c2d192e4b5a1d
shravan kumar <slmalviya32@gmail.com>
2024-02-21 20:12:00 +0530

    commit

---
+++ JavaScript/DOM/app.js
@@ -0,0 +1,8 @@
+let par = document.createElement("p");
+par.innerText = "Hey I am Shravan";
+document.querySelector("body").append(par);
+
+
+let head = document.createElement("h1");
+head.innerText = "Hey this Head";
+document.querySelector("body").append(head)
