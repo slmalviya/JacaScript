b1bde8c36375ee9a43dd29ee3257cfbb4ce46ddb
shravan kumar <slmalviya32@gmail.com>
2024-02-22 22:12:03 +0530

    commit

--- JavaScript/DOM/app.js
+++ JavaScript/DOM/app.js
@@ -1,8 +1,27 @@
-let par = document.createElement("p");
-par.innerText = "Hey I am Shravan";
-document.querySelector("body").append(par);
+// let par = document.createElement("p");
+// par.innerText = "Hey I am Shravan";
+// document.querySelector("body").append(par);
 
 
-let head = document.createElement("h1");
-head.innerText = "Hey this Head";
-document.querySelector("body").append(head)
+// let head = document.createElement("h1");
+// head.innerText = "Hey this Head";
+// document.querySelector("body").append(head)
+
+// Mouse Event OnClick
+
+// let click = document.querySelector("button");
+// click.onclick = sayHello;
+
+// function sayHello() {
+//     console.log("hello click me");
+// };
+
+// addEventListner
+
+
+let click = document.querySelector("button");
+click.addEventListener("click", sayHello);
+
+function sayHello() {
+    alert("Haanji hum dab gye haii chod do");
+}
