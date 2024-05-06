2a77bd6b7dad2a3538bb39157799c56a2b2e7016
shravan kumar <slmalviya32@gmail.com>
2024-05-06 13:38:36 +0530

    commit

--- JavaScript/Functions/promises.js
+++ JavaScript/Functions/promises.js
@@ -1,18 +1,34 @@
 function savetoDB(data, success, failure) {
   let internetSpeed = Math.floor(Math.random() * 10) + 1;
   if (internetSpeed > 4) {
-    succes();
+    success();
   } else {
     failure();
   }
 }
 
 savetoDB(
-    "Poornima",
+    "poornima",
     () =>{
-        console.log("Success: data1, was saved:");
+        console.log("Success: data was saved");
+    
+   savetoDB( "Shravan",
+    () =>{
+        console.log("Success2:  data was saved");
+    },
+    ()=>{
+        console.log("Failure2: data was not saved");
+    })
+    savetoDB( "College",
+    ()=>{
+       console.log("Success3: data was saved"); 
     }
-    savetoDB(
-
-    )
-)
+    ()=>
+        {
+            console.log("Failure3 : data was not saved");
+        }
+)}
+() =>{
+    console.log("failed: weak connection data is not saved");
+}
+);
