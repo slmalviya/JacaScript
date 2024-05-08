17f04e9eb0021ca89869aa3d415c80bc05ecda35
shravan kumar <slmalviya32@gmail.com>
2024-05-08 16:40:35 +0530

    commit

--- JavaScript/Functions/promises.js
+++ JavaScript/Functions/promises.js
@@ -1,3 +1,38 @@
+// function savetoDB(data, success, failure) {
+//   let internetSpeed = Math.floor(Math.random() * 10) + 1;
+//   if (internetSpeed > 4) {
+//     success();
+//   } else {
+//     failure();
+//   }
+// }
+
+// savetoDB(
+//     "poornima",
+//     () =>{
+//         console.log("Success: data was saved");
+    
+//    savetoDB( "Shravan",
+//     () =>{
+//         console.log("Success2:  data was saved");
+//     },
+//     ()=>{
+//         console.log("Failure2: data was not saved");
+//     })
+//     savetoDB( "College",
+//     ()=>{
+//        console.log("Success3: data was saved"); 
+//     }
+//     ()=>
+//         {
+//             console.log("Failure3 : data was not saved");
+//         }
+// )}
+// () =>{
+//     console.log("failed: weak connection data is not saved");
+// }
+// );
+
 function savetoDB(data, success, failure) {
   let internetSpeed = Math.floor(Math.random() * 10) + 1;
   if (internetSpeed > 4) {
@@ -6,29 +41,3 @@ function savetoDB(data, success, failure) {
     failure();
   }
 }
-
-savetoDB(
-    "poornima",
-    () =>{
-        console.log("Success: data was saved");
-    
-   savetoDB( "Shravan",
-    () =>{
-        console.log("Success2:  data was saved");
-    },
-    ()=>{
-        console.log("Failure2: data was not saved");
-    })
-    savetoDB( "College",
-    ()=>{
-       console.log("Success3: data was saved"); 
-    }
-    ()=>
-        {
-            console.log("Failure3 : data was not saved");
-        }
-)}
-() =>{
-    console.log("failed: weak connection data is not saved");
-}
-);
